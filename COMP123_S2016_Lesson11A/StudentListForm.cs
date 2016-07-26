8eeb6d16ef9aa5836abfd1c98350bed9c882e948
gapillan <gapillan@my.centennialcollege.ca>
2016-07-26 14:06:52 -0400

    added a student form list and stuff

---
+++ COMP123_S2016_Lesson11A/StudentListForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_S2016_Lesson11A
+{
+    public partial class StudentListForm : Form
+    {
+        public StudentListForm()
+        {
+            InitializeComponent();
+        }
+
+        private void StudentListForm_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'cOMP123DataSet.Students' table. You can move, or remove it, as needed.
+            this.studentsTableAdapter.Fill(this.cOMP123DataSet.Students);
+
+        }
+
+        private void StudentListLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void AddStudentButton_Click(object sender, EventArgs e)
+        {
+            AddStudentForm addStudentForm = new AddStudentForm();
+            addStudentForm.studentListForm = this;
+            addStudentForm.Show();
+            this.Hide();
+        }
+    }
+}
